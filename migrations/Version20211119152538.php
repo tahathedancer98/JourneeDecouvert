f7809ea2672d1b05bcd80d4d3e4860199915ab0d
tahathedancer98 <bookingtaha98@gmail.com>
2021-11-19 17:17:16 +0100

    List all Comments + add new one

--- migrations/Version20211119152538.php
+++ migrations/Version20211119152538.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211119100056 extends AbstractMigration
+final class Version20211119152538 extends AbstractMigration
 {
     public function getDescription(): string
     {
